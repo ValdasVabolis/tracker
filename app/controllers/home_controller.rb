605e9356aa94ddc50250a1cfb4a165883eaa8b06
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2019-01-01 09:54:53 +0200

    Add average stat

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   def index
     @total = Measurement.sum(:reps)
     @measurement = Measurement.new
+    measurement_count = Measurement.count
+    if measurement_count > 0
+      @average = @total / Measurement.count
+    else
+      @average = 0
+    end
   end
 end
