b7486ba6ff7151524b3fb159bf48a6f2a9e7c809
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2019-01-01 21:34:25 +0200

    Add measurements history

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,4 +1,8 @@
 class MeasurementsController < ApplicationController
+  def index
+    @measurements = Measurement.all
+  end
+
   def create
     @measurement = Measurement.new(params.require(:measurement).permit(:reps))
     if @measurement.save
