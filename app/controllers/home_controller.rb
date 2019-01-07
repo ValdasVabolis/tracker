9ebde8aa37aa842cf9f3a3fc6b4ad137d6aba7ca
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2019-01-07 21:50:40 +0200

    Add another average stat: average per day

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,10 @@ class HomeController < ApplicationController
     @total = Measurement.sum(:reps)
     @measurement = Measurement.new
     measurement_count = Measurement.count
+    @average_per_day = (@total / Measurement.all.group_by { |m| m.created_at.day }.size.to_f).round(2) 
+    @average_per_set = 0
     if measurement_count > 0
-      @average = (@total / Measurement.count.to_f).round(2)
-    else
-      @average = 0
+      @average_per_set = (@total / measurement_count.to_f).round(2)
     end
   end
 end
